3ca99ffd364ea201e16803e4084aa66340dcfa55
Stephen Tawiah <stawiah17@gmail.com>
2022-12-10 12:39:04 -0800

    shortened print statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
+pos_statement = "Last digit of {} is {} and is greater than."
+zero_statement = "Last digit of {} is {} and is 0."
+else_statement = "Last digit of {} is {} and is less than 6 and not 0."
 if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+    print(pos_statement.format(number, last_digit))
 elif last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+    print(zero_statement.format(number, last_digit))
 elif (last_digit < 6) and (last_digit != 0):
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+    print(else_statement.format(number, last_digit))
